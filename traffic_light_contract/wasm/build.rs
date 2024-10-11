31c9fd319ccb36484bb42e3941cb0cfa92f88831
David-HernandezM <dh29550@gmail.com>
2024-10-11 01:54:51 -0600

    removed unnecessary comments

--- traffic_light_contract/wasm/build.rs
+++ traffic_light_contract/wasm/build.rs
@@ -3,21 +3,6 @@ use std::{env, fs, path::PathBuf};
 use app::TrafficLightProgram;
 
 fn main() {
-    // gwasm_builder::build();
-
-    // let manifest_dir_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-
-    // let idl_file_path = manifest_dir_path.join("template.idl");
-
-    // let idl_file = File::create(idl_file_path).unwrap();
-
-    // program::generate_idl::<TrafficLightProgram>(idl_file).unwrap();
-
-
-
-
-
-
     // Build contract to get .opt.wasm
     sails_rs::build_wasm();
 
